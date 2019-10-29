baab1dd9ff69a7643f54bfaa9fa027945ec68e2b
Simon Farrow <Simon.Farrow@rullion.co.uk>
2019-10-29 09:29:08 +0000

    trying to save something to a table

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,11 +1,28 @@
-﻿module.exports = function (context, req) {
+﻿const azure = require('azure-storage');
+const uuid = require('uuid/v1');
+
+const tableService = azure.createTableService();
+const tableName = "mytable";
+
+module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name) + " we can deploy"
-        };
+        
+        const item = (req.query.name || req.body.name);
+        item["PartitionKey"] = "Partition";
+        item["RowKey"] = uuid();
+
+        // Use { echoContent: true } if you want to return the created item including the Timestamp & etag
+        tableService.insertEntity(tableName, item, { echoContent: true }, function (error, result, response) {
+            if (!error) {
+                context.res.status(201).json(response);
+            } else {
+                context.res.status(500).json({ error: error });
+            }
+        });
+
+        context.res.body = "Hello " + (req.query.name || req.body.name) + " we have saved something";
     }
     else {
         context.res = {
